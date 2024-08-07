74a95a155e971e94ccc1471059633dc017fa2450
Graham Lutz <graham.lutz@plethora.com>
2024-08-07 09:09:18 -0400

    removes no sandbox args

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -6,7 +6,6 @@ class Page {
   static async build() {
     const browser = await pup.launch({
       headless: false,
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
       env: { DISPLAY: ":10.0" }
     });
 
